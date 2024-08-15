f0c62d46ee054cf441539d9ec09f9d7566cb747c
Elín Ósk <elinosk@elinosk.com>
2024-08-15 22:57:24 +0000

    [Feature] Password validation set up

--- Admin/Managers/ValidationManager.swift
+++ Admin/Managers/ValidationManager.swift
@@ -9,7 +9,6 @@ import Foundation
 
 extension String {
   func isValidEmail() -> Bool {
-      // here, `try!` will always succeed because the pattern is valid
     let regex = try! NSRegularExpression(pattern: "^[a-zA-Z0-9.-]+@([a-zA-Z0-9-]+.)+[a-zA-Z0-9]{2,4}$", options: .caseInsensitive)
     return regex.firstMatch(in: self, options: [], range: NSRange(location: 0, length: count)) != nil
   }
@@ -19,10 +18,20 @@ final class ValidationManager {
   static let shared = ValidationManager()
   private init() {}
 
-  func validateEmail(_ email: String) throws -> Bool {
+  func validateEmail(_ email: String) -> Bool {
     if email.isEmpty {
       return false
     }
     return email.isValidEmail()
+  }
+
+  func validatePassword(_ password: String) -> Bool {
+    if password.isEmpty {
+      return false
+    }
+    if password.count < 8 {
+      return false
+    }
+    return true
   }
 }
